983ab77d443faaabed2561055f6e37e46d4310da
dhruvrijwani <dhruvrijwani@gmail.com>
2026-01-16 16:25:38 +0530

    homepage done

--- app/Components/Navbar/Navbar.jsx
+++ app/Components/Navbar/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
     },
     // { label: "Press", href: "/press" },
     { label: "Contact Us", href: "/contactus" },
-    { label: "Blog", href: "/blog" },
+    { label: "Blog", href: "/blogs" },
   ];
 
   const [menuOpen, setMenuOpen] = useState(false);
